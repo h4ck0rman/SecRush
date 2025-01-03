70159dbd0bf62bf93625409ac4a464b28719c0ab
h4ck0rman <h4ck0rman@gmail.com>
2025-01-02 20:21:24 -0500

    final touches pre-deployment

--- src/app/src/routes/+page.svelte
+++ src/app/src/routes/+page.svelte
@@ -1,35 +1,11 @@
-<div class="flex items-center justify-center h-screen w-screen">
-    <h1 class="text-2xl text-white text-center w-full text-bold uppercase">Application Security Rush</h1>
+<div class="hero-container h-screen w-full bg-black flex flex-col items-center justify-center relative overflow-hidden">
+
+    <a
+        href="/rush"
+        class="border font-mono border-white hover:bg-white text-white cursor-pointer transition duration-150 flex items-center justify-center w-1/2 py-4 hover:text-black text-4xl lowercase rounded-2xl"
+    >
+        SecRush puzzle
+    </a>
+
 </div>
-<script>
-    import { startSession } from '../lib/api'; 
-    import { goto } from '$app/navigation';
 
-    let errorMessage = '';
-
-    const handleStartGame = async () => {
-        try {
-            const sessionId = await startSession();
-            console.log('Session started with ID:', sessionId);
-
-            // Navigate to the puzzle page
-            goto(`/rush`);
-        } catch (error) {
-            errorMessage = 'Failed to start the session. Please try again later.';
-        }
-    };
-</script>
-
-<main>
-    <h1>Start Your Puzzle Game</h1>
-    <button on:click={handleStartGame}>Start Game</button>
-    {#if errorMessage}
-        <p class="error">{errorMessage}</p>
-    {/if}
-</main>
-
-<style>
-    .error {
-        color: red;
-    }
-</style>
