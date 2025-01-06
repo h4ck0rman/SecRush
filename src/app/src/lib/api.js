1b63a54a8e067cddd7e6b396ae03b15614693bff
h4ck0rman <h4ck0rman@gmail.com>
2025-01-06 02:59:07 -0500

    minor switches

--- src/app/src/lib/api.js
+++ src/app/src/lib/api.js
@@ -1,6 +1,6 @@
 export async function startSession() {
     try {
-        const backend = "https://api.sec-rush.com";
+        const backend = import.meta.env.VITE_PUBLIC_API_DOMAIN ?? "https://api.sec-rush.com";
         // Make a POST request to the "start session" API endpoint
         const response = await fetch(backend + '/gameSession/newSession', {
             method: 'POST',
