32d7e78d86d721bdc906433a2d22a051fa06171f
h4ck0rman <h4ck0rman@gmail.com>
2025-01-06 05:19:10 -0500

    decreased padding on puzzle results

--- src/app/src/routes/rush/puzzleResult.svelte
+++ src/app/src/routes/rush/puzzleResult.svelte
@@ -5,10 +5,10 @@
 </script>
 
 <!-- Container -->
-<div class="flex flex-nowrap md:flex-wrap overflow-x-auto md:overflow-x-visible w-full px-5 py-2">
+<div class="flex flex-nowrap md:flex-wrap overflow-x-auto md:overflow-x-visible w-full px-5 py-1">
     {#each results as result}
         <!-- Individual Result Item -->
-        <div class="flex flex-col items-center px-2 py-2 flex-shrink-0">
+        <div class="flex flex-col items-center px-2 py-1 flex-shrink-0">
             <img src="{result.correct ? correct : wrong}" alt="result logo" class="w-6 h-6 mb-1" />
             <p class="text-white font-bold">{result.count}</p>
         </div>
